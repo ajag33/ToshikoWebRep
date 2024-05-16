4331d05fbe09f0de40dbdcae0c324413a8ba2be0
ajag33 <55101799+ajag33@users.noreply.github.com>
2024-05-16 11:40:32 -0500

    Update enviarcorreos.php
    correo 2

--- enviarcorreos.php
+++ enviarcorreos.php
@@ -44,12 +44,12 @@ try {
  //  $mail->SMTPSecure = "tls";
 //$mail->Port       = 587;     
 
-           $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'grupotoshiko22@gmail.com';                    //SMTP username
-    $mail->Password   = 'boccyvhiuqxtprkr';                              //SMTP password
+    $mail->Username   = 'toshikodenuncias@gmail.com';                    //SMTP username
+    $mail->Password   = 'ttfjvsokrxuhsrix';                              //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;  
         //hoy
